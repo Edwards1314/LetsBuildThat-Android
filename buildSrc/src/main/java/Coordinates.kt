a9c2d4284029db8488ab3671ab71fb2f5bcd9822
Edwards1314 <36805894+Edwards1314@users.noreply.github.com>
2020-04-25 13:05:39 -0500

    Update Coordinates.kt

--- buildSrc/src/main/java/Coordinates.kt
+++ buildSrc/src/main/java/Coordinates.kt
@@ -1,7 +1,7 @@
-val PUBLISHING_GROUP = "com.ncorti.kotlin.template"
+val PUBLISHING_GROUP = "com.letsbuildthat"
 
 object AppCoordinates {
-    const val APP_ID = "com.ncorti.kotlin.template.app"
+    const val APP_ID = "com.letsbuildthat"
 
     const val APP_VERSION_NAME = "1.0.0"
     const val APP_VERSION_CODE = 1
